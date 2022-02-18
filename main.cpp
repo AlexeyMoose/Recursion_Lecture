26acdd8ff6d16356947889aa91901ede7a1ea0c2
AlexeyMoose <59811982+AlexeyMoose@users.noreply.github.com>
2022-02-18 13:18:18 -0600

    Lecture
    should have done this on the develop branch. Bad alexey

--- main.cpp
+++ main.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
 
+// Recursive function: Calculate the nth Fibonacci Number
+// fib(n) = 1 1 2 3 5 8 13...
+//      n = 1 2 3 4 5 6 7...
+
+/**
+ * @brief Calculate the nth fib number
+ * 
+ * @param n designates which number in the sequence we desire, start
+ * @return int 
+ */
+int fib(int n);
+// This is called a declaration!
+
 int main ( )
 {
     std::cout << "Hello, World!" << std::endl;
+    for (int i{0}; i < 6; ++i)
+    {
+        std::cout << "fib(" << i << ") = " << fib(i) << std::endl;
+    }
     return 0;
+}
+
+int fib(int n)
+{
+    if (n == 0 or n == 1)
+    {
+        // this is our base case
+        return n;
+    }
+    return fib(n - 1) + fib(n - 2);
 }
